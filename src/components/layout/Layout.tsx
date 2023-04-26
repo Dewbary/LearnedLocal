67a97d366839ae2a7a14d444a8256c7513cb2044
Chase Maxfield <maxfield.chase@gmail.com>
2023-04-26 15:36:31 -0600

    bugfix

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,8 @@
 import React, { PropsWithChildren } from "react";
-import { Header } from "../Header";
 
 const Layout = (props: PropsWithChildren) => {
   return (
     <div className="grid min-h-screen grid-rows-header bg-white">
-      {/* <div className="z-10 bg-white shadow-sm">
-        <Header />
-      </div> */}
       <div>{props.children}</div>
     </div>
   );
