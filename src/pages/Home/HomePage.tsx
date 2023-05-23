d51bd023405836aae2be50a49fa0f04b72d97937
Chase Maxfield <maxfield.chase@gmail.com>
2023-05-23 11:21:38 -0600

    bugfix

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -66,7 +66,7 @@ const HomePage = () => {
         <div className="mx-10 my-10 flex flex-col items-center">
           <h2 className="text-5xl lg:text-7xl font-bold">What is Learned Local?</h2>
           <h3 className="mt-6 text-xl lg:mx-10">
-            We believe that everyone has hobbies, passions, and interests that makes them unique. We made Learned Local as a place for members of your community to come together and share these interests with each other, in events that we like to call "experiences". Along the way, you'll forge friendships, create memories, and discover that you have a lot more in common with the people around you than you think. Sign up for an experience today!
+            We believe that everyone has hobbies, passions, and interests that makes them unique. We made Learned Local as a place for members of your community to come together and share these interests with each other, in events that we like to call &quot;experiences&quot;. Along the way, you&apos;ll forge friendships, create memories, and discover that you have a lot more in common with the people around you than you think. Sign up for an experience today!
           </h3>
           <video
             src="/learnedlocalvid.mp4"
