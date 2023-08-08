302c8bf1f1fc04cc97d62f055d89c67e823e19b0
Dewbary <brendandewberry99@gmail.com>
2023-08-07 19:14:01 -0600

    add google tag manager code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,15 @@ export default function Document() {
   return (
     <Html data-theme="bumblebee">
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P4NKHQ58'`,
+          }}
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;900&display=swap"
           rel="stylesheet"
@@ -31,6 +40,12 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4NKHQ58"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
       </body>
     </Html>
   );
