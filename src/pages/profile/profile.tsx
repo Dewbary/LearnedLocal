cddd2e587ef0f83100c99b0b93b85880b1600db8
Chase Maxfield <maxfield.chase@gmail.com>
2023-09-08 12:44:55 -0600

    profile fix and minor homepage fix

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,4 +1,4 @@
-import { useUser } from "@clerk/nextjs";
+import { RedirectToSignIn, SignedIn, SignedOut, useUser } from "@clerk/nextjs";
 import Head from "next/head";
 import Footer from "~/components/Footer/Footer";
 import NavBar from "~/components/NavBar/NavBar";
@@ -85,6 +85,8 @@ export default function Profile () {
                 phone: phone,
                 profileImage: profileImage,
             });
+
+            router.reload();
         }
         
     }
@@ -119,75 +121,83 @@ export default function Profile () {
                 <title>Learned Local</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            
             <NavBar isSignedIn={user.isSignedIn || false} className="bg-white drop-shadow-md"/>
 
-            <div className="flex flex-col w-full gap-10 min-h-screen pb-10 pt-14 lg:pt-0">
-                <div className="flex w-full justify-center py-10 bg-gradient-to-r from-amber-400 via-amber-200 to-amber-50">
-                    <h1 className="text-3xl lg:text-5xl font-bold">Profile and Settings</h1>
-                </div>
+            <SignedIn>
+                <div className="flex flex-col w-full gap-10 min-h-screen pb-10 pt-14 lg:pt-0">
+                    <div className="flex w-full justify-center py-10 bg-gradient-to-r from-amber-400 via-amber-200 to-amber-50">
+                        <h1 className="text-3xl lg:text-5xl font-bold">Profile and Settings</h1>
+                    </div>
 
-                <div className="flex w-full gap-10 flex-col lg:flex-row">
-                    <div className="lg:basis-1/3 flex flex-col lg:items-end gap-2 lg:gap-5 lg:mt-5 mx-5 lg:mx-0">
-                        {Tabs.map(tab => (
-                            <button 
-                                className={`flex items-center gap-2 px-3 py-2 rounded-md ${selectedTab?.label === tab.label ? "bg-amber-400 text-white" : ""}`}
-                                key={tab.label}
-                                onClick={() => setSelectedTab(tab)}
+                    <div className="flex w-full gap-10 flex-col lg:flex-row">
+                        <div className="lg:basis-1/3 flex flex-col lg:items-end gap-2 lg:gap-5 lg:mt-5 mx-5 lg:mx-0">
+                            {Tabs.map(tab => (
+                                <button 
+                                    className={`flex items-center gap-2 px-3 py-2 rounded-md ${selectedTab?.label === tab.label ? "bg-amber-400 text-white" : ""}`}
+                                    key={tab.label}
+                                    onClick={() => setSelectedTab(tab)}
+                                >
+                                    <span>{tab.label}</span>
+                                    {tab.icon}
+                                </button>
+                            ))}
+                            <hr className="w-full lg:w-36" />
+                            <button
+                                disabled={firstName.length <= 0 || lastName.length <= 0} 
+                                className="px-3 py-2 bg-green-400 rounded-md text-white drop-shadow-md disabled:bg-gray-400 hover:bg-green-500" 
+                                onClick={() => handleSaveClick()}
                             >
-                                <span>{tab.label}</span>
-                                {tab.icon}
+                                {profileExists === "yes" ? "Save Changes" : "Create Profile"} 
                             </button>
-                        ))}
-                        <hr className="w-full lg:w-36" />
-                        <button
-                            disabled={firstName.length <= 0 || lastName.length <= 0} 
-                            className="px-3 py-2 bg-green-400 rounded-md text-white drop-shadow-md disabled:bg-gray-400" 
-                            onClick={() => handleSaveClick()}
-                        >
-                            {profileExists === "yes" ? "Save Changes" : "Create Profile"} 
-                        </button>
-                        <p 
-                            hidden={!(firstName.length <= 0 || lastName.length <= 0)} 
-                            className="text-red-600 w-full lg:w-48 text-center lg:text-right"
-                        >
-                            You must provide a first name and a last name.
-                        </p>
-                    </div>
-                    <div className="basis-2/3">
-                        {selectedTab?.label === "Basic Info" &&
-                            <BasicInfo 
-                                firstName={firstName} 
-                                setFirstName={setFirstName} 
-                                lastName={lastName} 
-                                setLastName={setLastName} 
-                                bio={bio} 
-                                setBio={setBio} 
-                                social={social}
-                                setSocial={setSocial}
-                                email={email}
-                                setEmail={setEmail}
-                                phone={phone}
-                                setPhone={setPhone}
-                                profileImage={profileImage}
-                                setProfileImage={setProfileImage}
-                            />
-                        }
-                        {selectedTab?.label === "Payment Info" &&
-                            <PaymentInfo
-                                venmoAccount={venmoAccount}
-                                setVenmoAccount={setVenmoAccount}
-                                zelleAccount={zelleAccount}
-                                setZelleAccount={setZelleAccount}
-                            />
-                        }
-                        {selectedTab?.label === "Advanced Settings" &&
-                            <AdvancedSettings
-                                deleteAccountAction={deleteAccountAction}
-                            />
-                        }
+                            <p 
+                                hidden={!(firstName.length <= 0 || lastName.length <= 0)} 
+                                className="text-red-600 w-full lg:w-48 text-center lg:text-right"
+                            >
+                                You must provide a first name and a last name.
+                            </p>
+                        </div>
+                        <div className="basis-2/3">
+                            {selectedTab?.label === "Basic Info" &&
+                                <BasicInfo 
+                                    firstName={firstName} 
+                                    setFirstName={setFirstName} 
+                                    lastName={lastName} 
+                                    setLastName={setLastName} 
+                                    bio={bio} 
+                                    setBio={setBio} 
+                                    social={social}
+                                    setSocial={setSocial}
+                                    email={email}
+                                    setEmail={setEmail}
+                                    phone={phone}
+                                    setPhone={setPhone}
+                                    profileImage={profileImage}
+                                    setProfileImage={setProfileImage}
+                                />
+                            }
+                            {selectedTab?.label === "Payment Info" &&
+                                <PaymentInfo
+                                    venmoAccount={venmoAccount}
+                                    setVenmoAccount={setVenmoAccount}
+                                    zelleAccount={zelleAccount}
+                                    setZelleAccount={setZelleAccount}
+                                />
+                            }
+                            {selectedTab?.label === "Advanced Settings" &&
+                                <AdvancedSettings
+                                    deleteAccountAction={deleteAccountAction}
+                                />
+                            }
+                        </div>
                     </div>
                 </div>
-            </div>
+            </SignedIn>
+            <SignedOut>
+                <RedirectToSignIn />
+            </SignedOut>
+
+            
 
             <Footer />
         </div>
