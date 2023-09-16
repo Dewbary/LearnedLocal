d49327384c5d48aa57642a2b1e92de641107e6fe
Dewbary <brendandewberry99@gmail.com>
2023-09-15 20:35:42 -0600

    add sanity cms for blog

---
+++ src/pages/blog.tsx
@@ -0,0 +1,40 @@
+import { client } from "../utils/sanityClient";
+import { GetStaticProps } from "next";
+
+type Post = {
+  _id: string;
+  title?: string;
+  slug?: {
+    current: string;
+  };
+};
+
+type Props = {
+  posts: Post[];
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await client.fetch<Post[]>(`*[_type == "post"]`);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+const Blog = ({ posts }: Props) => {
+  if (!posts) return <div>Loading...</div>;
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post._id}>
+          <a href={post?.slug?.current}>{post?.title}</a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Blog;
