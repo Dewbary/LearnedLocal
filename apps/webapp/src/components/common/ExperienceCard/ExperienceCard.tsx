6cdd6cb6edd022d91c7c65390d084a7443afabaa
Chase Maxfield <maxfield.chase@gmail.com>
2023-09-26 20:54:16 -0600

    myexperiences bugfixes

--- apps/webapp/src/components/common/ExperienceCard/ExperienceCard.tsx
+++ apps/webapp/src/components/common/ExperienceCard/ExperienceCard.tsx
@@ -23,31 +23,38 @@ export default function ExperienceCard({
   const [isLoading, setLoading] = React.useState(true);
 
   return (
-    <CustomModal
-      button={
-        <div className="group">
-          <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg xl:aspect-w-7 xl:aspect-h-8">
-            <Image
-              alt="experience photo"
-              src={experience.photos[0] || ""}
-              fill
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              className={cn(
-                "object-cover",
-                "duration-700 ease-in-out group-hover:opacity-75",
-                isLoading
-                  ? "scale-110 blur-2xl grayscale"
-                  : "scale-100 blur-0 grayscale-0"
-              )}
-              onLoadingComplete={() => setLoading(false)}
-            />
-          </div>
-          <CardInfo experience={experience} />
+    <div>
+      <div className="group">
+          <CustomModal
+            button={
+              <>
+                <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg xl:aspect-w-7 xl:aspect-h-8">
+                  <Image
+                    alt="experience photo"
+                    src={experience.photos[0] || ""}
+                    fill
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    className={cn(
+                      "object-cover",
+                      "duration-700 ease-in-out group-hover:opacity-75 group-hover:cursor-pointer",
+                      isLoading
+                        ? "scale-110 blur-2xl grayscale"
+                        : "scale-100 blur-0 grayscale-0"
+                    )}
+                    onLoadingComplete={() => setLoading(false)}
+                  />
+                </div>
+                <CardInfo className="group-hover:cursor-pointer" experience={experience} />
+              </>
+            }
+          >
+            {getModalImpl(experience, registered)}
+          </CustomModal>
+        
+        <div className="flex flex-row">
           {children}
         </div>
-      }
-    >
-      {getModalImpl(experience, registered)}
-    </CustomModal>
+      </div>
+    </div>
   );
 }
