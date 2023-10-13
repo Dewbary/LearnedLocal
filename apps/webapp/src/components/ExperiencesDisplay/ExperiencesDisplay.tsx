5715656fd7f5bb3a114a35e813d2b5ca3197dba7
Dewbary <brendandewberry99@gmail.com>
2023-10-13 12:02:47 -0600

    update experience card css

--- apps/webapp/src/components/ExperiencesDisplay/ExperiencesDisplay.tsx
+++ apps/webapp/src/components/ExperiencesDisplay/ExperiencesDisplay.tsx
@@ -12,12 +12,18 @@ const ExperiencesDisplay = () => {
       <div className="mx-auto mt-4 mb-4 max-w-2xl px-8 sm:px-4 md:mb-8 md:px-0 lg:max-w-full lg:px-8">
         <div
           className={`grid grid-cols-1 gap-y-10 gap-x-6 ${
-            styles.autofit ?? ""
+            filteredExperiences.length > 3
+              ? styles.autofit ?? ""
+              : styles.autofitSmaller ?? ""
           } xl:gap-x-8`}
         >
           {filteredExperiences &&
             filteredExperiences.map((experience) => (
-              <ExperienceCard key={experience.id} experience={experience} isHomePageCard={true}/>
+              <ExperienceCard
+                key={experience.id}
+                experience={experience}
+                isHomePageCard={true}
+              />
             ))}
         </div>
       </div>
