3a87b0ca215361049ef7f643fe6da0baa742409f
Chase Maxfield <maxfield.chase@gmail.com>
2023-10-31 11:34:31 -0600

    fixed stuff

--- packages/api/routers/textlist.ts
+++ packages/api/routers/textlist.ts
@@ -7,12 +7,12 @@ export const textListRouter = createTRPCRouter({
         .input(z.string())
         .mutation(async ({ctx, input}) => {
 
-            // await sendTextMessage(input, "Hey, this is LearnedLocal. Thanks for signing up for our date / hobby idea list! We'll keep you posted about experiences available in Utah County.");
-            // await ctx.prisma.phoneContact.create({
-            //     data: {
-            //         phoneNumber: input
-            //     }
-            // });
+            await sendTextMessage(input, "Hey, this is LearnedLocal. Thanks for signing up for our date / hobby idea list! We'll keep you posted about experiences available in Utah County.");
+            await ctx.prisma.phoneContact.create({
+                data: {
+                    phoneNumber: input
+                }
+            });
             console.log(`Added ${input} to the text list!`);
         })
 })
