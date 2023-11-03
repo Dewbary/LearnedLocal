d4f5be6777b9f13f2b5df863339c193fd3a5b52a
Dewbary <brendandewberry99@gmail.com>
2023-11-03 14:59:34 -0600

    update import

--- apps/webapp/src/pages/newhost/newhost.tsx
+++ apps/webapp/src/pages/newhost/newhost.tsx
@@ -1,19 +1,18 @@
-import NavBar from "~/components/Landing/NavBar";
 import { useUser } from "@clerk/nextjs";
 import HostBody from "~/components/Host/HostBody";
 import Footer from "~/components/Landing/Footer";
+import NewNavBar from "~/components/NewNavBar";
 
 export default function NewHost() {
+  const { isSignedIn } = useUser();
 
-    const { isSignedIn } = useUser();
-
-    return (
-        <>
-            <div className="flex flex-col bg-ll-grey items-center min-h-screen w-full">
-                <NavBar isSignedIn={isSignedIn || false}/>
-                <HostBody />
-                <Footer />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex min-h-screen w-full flex-col items-center bg-ll-grey">
+        <NewNavBar isSignedIn={isSignedIn || false} />
+        <HostBody />
+        <Footer />
+      </div>
+    </>
+  );
 }
