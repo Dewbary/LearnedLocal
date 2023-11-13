6ed72efd7a4520dcaf2a6a1b6dcc90ff46efe1b9
Chase Maxfield <maxfield.chase@gmail.com>
2023-11-13 12:59:48 -0700

    About page added to website

--- apps/webapp/src/pages/about/about.tsx
+++ apps/webapp/src/pages/about/about.tsx
@@ -1,47 +1,102 @@
+import NewNavBar from "~/components/NewNavBar";
 import { useUser } from "@clerk/nextjs";
-import * as React from "react";
-import NavBar from "~/components/NavBar/NavBar";
+import Footer from "~/components/NewFooter/NewFooter";
+import Image from "next/image";
+import aaronArch from "../../../assets/about/aaron_arch_image.jpg";
+import aaronImage from "../../../assets/about/aaron_image.jpg";
+import brendanImage from "../../../assets/about/brendan_image.jpeg";
+import chaseImage from "../../../assets/about/chase_image.jpg";
+import graceImage from "../../../assets/about/grace_image.jpg";
 
-const AboutPage = () => {
-  const user = useUser();
+export default function AboutPage () {
+
+  const { isSignedIn } = useUser();
 
   return (
-    <div className="flex min-h-screen w-full flex-col bg-white">
-      <NavBar isSignedIn={user.isSignedIn ?? false} className="bg-white" />
-      <div className="mx-10 mt-20 flex flex-col items-center">
-        <span id="aboutlearnedlocal" />
-        <h2 className="text-center text-4xl font-bold lg:text-7xl">
-          What is Learned Local?
-        </h2>
-        <p className="mt-6 text-left text-xl lg:mx-24">
-          Having a hard time thinking of date night ideas? Looking for fun
-          things to do in Utah and Provo? Why not learn a new hobby from a
-          talented individual or business right where you live? Come find an
-          experience and create a memory with us while building a stronger sense
-          of community!
-        </p>
-        <p className="mt-6 text-left text-xl lg:mx-24">
-          We believe that everyone has hobbies, passions, and interests that
-          makes them unique. We made Learned Local as a place for members of
-          your community to come together and share these interests with each
-          other, in events that we like to call &quot;experiences&quot;. Along
-          the way, you&apos;ll forge friendships, create memories, and discover
-          that you have a lot more in common with the people around you than you
-          think. Sign up for an experience today!
-        </p>
-
-        <div className="mt-10 flex w-full items-center justify-center mb-10">
-          <div className="aspect-w-16 aspect-h-9 lg:aspect-h-5 flex w-full items-center justify-center max-w-3xl max-h-48">
-            <iframe
-              src="https://www.youtube.com/embed/leKfHxT_6II"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-            ></iframe>
+    <>
+      <div className="w-full min-h-screen flex flex-col items-center bg-ll-grey">
+        <NewNavBar isSignedIn={isSignedIn || false} isMarketingNavBar={true}/>
+        <div className="px-5 flex flex-col gap-8 pt-5 pb-16 w-full lg:max-w-5xl">
+          <div className="text-3xl font-raleway font-semibold">Our Story</div>
+          <div className="flex flex-col gap-8 w-full lg:flex-row lg:items-center lg:gap-12">
+            <div className="w-full lg:basis-1/3">
+              <div className="relative aspect-[5/7] overflow-hidden rounded-2xl">
+                <Image
+                  src={aaronArch}
+                  alt="Aaron Jarrett, founder of Learned Local"
+                  fill
+                  className="object-cover"
+                />
+              </div>
+            </div>
+            <div className="font-inter text-sm flex flex-col gap-3 font-light lg:basis-2/3 lg:text-base">
+              <p>Hello, my name is AJ and I am the founder of Learned Local. My passion for connecting with others and creating unique experiences started in 2019 as a tour guide in Alaska. I was amazed to see so many travel from all over the world, seeking to create unforgettable memories. As someone who couldn&apos;t afford to travel far, I began to wonder if instead I could find fun things to do to do in my own community. Turns out there wasn&apos;t a very good platform to do that, so I met some incredible cofounders that turned my dreams into a reality.</p>
+              <p>Learned Local is the platform where talented people can share their passion without all the busy work of obtaining a business license, paying for and creating a website, handing all the payment, and scheduling. We do all the hard work so you can focus on doing what you love.</p>
+              <p>I believe everyone has something to share with the world, and I really hope we as a community can step out of our shells and celebrate our differences through learning from each other. If you&apos;re looking to create unforgettable memories, support local individuals, and join an inviting community, you are in the right place. Welcome!</p>
+            </div>
+          </div>
+          <div className="flex flex-col bg-ll-tan items-center rounded-3xl gap-10 p-10 lg:pb-16 w-full">
+            <div className="text-3xl font-semibold font-raleway">
+              Our team
+            </div>
+            <div className="flex flex-col lg:flex-row items-center gap-8 lg:gap-12">
+              <div className="flex flex-col items-center gap-5">
+                <Image
+                  src={aaronImage}
+                  alt="Aaron Jarrett"
+                  width={180}
+                  height={180}
+                  className="rounded-2xl"
+                />
+                <div className="flex flex-col items-center font-inter">
+                  <div className="font-bold">Aaron Jarrett</div>
+                  <div>Founder</div>
+                </div>
+              </div>
+              <div className="flex flex-col items-center gap-5">
+                <Image
+                  src={brendanImage}
+                  alt="Brendan Dewberry"
+                  width={180}
+                  height={180}
+                  className="rounded-2xl"
+                />
+                <div className="flex flex-col items-center font-inter">
+                  <div className="font-bold">Brendan Dewberry</div>
+                  <div>Developer</div>
+                </div>
+              </div>
+              <div className="flex flex-col items-center gap-5">
+                <Image
+                  src={chaseImage}
+                  alt="Chase Maxfield"
+                  width={180}
+                  height={180}
+                  className="rounded-2xl"
+                />
+                <div className="flex flex-col items-center font-inter">
+                  <div className="font-bold">Chase Maxfield</div>
+                  <div>Developer</div>
+                </div>
+              </div>
+              <div className="flex flex-col items-center gap-5">
+                <Image
+                  src={graceImage}
+                  alt="Grace Sorensen"
+                  width={180}
+                  height={180}
+                  className="rounded-2xl"
+                />
+                <div className="flex flex-col items-center font-inter">
+                  <div className="font-bold">Grace Sorensen</div>
+                  <div>Designer</div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
-    </div>
+    </>
   );
 };
-
-export default AboutPage;
