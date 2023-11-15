5fc312ab83dce3d67f9b49c2685d3ff2037b5444
Dewbary <brendandewberry99@gmail.com>
2023-11-15 11:18:50 -0700

    Set default start and end times

--- apps/webapp/src/components/common/Fields/DatePickerField/DatePickerField.tsx
+++ apps/webapp/src/components/common/Fields/DatePickerField/DatePickerField.tsx
@@ -8,6 +8,7 @@ import { enUS } from "date-fns/locale";
 import type { DateInfo } from "~/components/types";
 import { produce } from "immer";
 import { sortByDate } from "../../DateAndTimePicker/DateAndTimeUtils";
+import { d } from "msw/lib/glossary-de6278a9";
 
 registerLocale("enUS", enUS);
 
@@ -26,10 +27,22 @@ const DatePickerField = ({ field, form }: FieldProps<DateInfo[]>) => {
       if (dateIndex > -1) {
         draft.splice(dateIndex, 1);
       } else {
-        draft.push({
+        const dateToAdd = {
           startTime: date,
           endTime: date,
-        });
+        };
+
+        // Set default start and end times to match the time already configured in the first availablity
+        if (
+          selectedDates[0] &&
+          selectedDates[0].startTime &&
+          selectedDates[0].endTime
+        ) {
+          dateToAdd.startTime.setTime(selectedDates[0].startTime.getTime());
+          dateToAdd.endTime.setTime(selectedDates[0].endTime.getTime());
+        }
+
+        draft.push(dateToAdd);
       }
     });
 
