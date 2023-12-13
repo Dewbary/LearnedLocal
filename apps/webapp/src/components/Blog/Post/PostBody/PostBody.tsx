efffb9620b231b85c4ee2019c0b2d6f38fb53d2e
Dewbary <68751677+Dewbary@users.noreply.github.com>
2023-12-12 17:15:30 -0700

    add outline to blog (#46)

--- apps/webapp/src/components/Blog/Post/PostBody/PostBody.tsx
+++ apps/webapp/src/components/Blog/Post/PostBody/PostBody.tsx
@@ -1,16 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import { PortableText } from "@portabletext/react";
 
-import { BlockContent } from "~/components/types";
 import markdownStyles from "./markdown-styles.module.css";
+import { BlockContent } from "~/components/types";
+import { SanityBlock } from "sanity-codegen";
+import Outline from "../../Outline";
 
 type Props = {
+  headings: SanityBlock[];
   content: BlockContent | undefined;
 };
 
-export default function PostBody({ content }: Props) {
+const components = {
+  block: {
+    h1: (props: any) => {
+      const { node, children } = props;
+      console.log(node.children);
+      return <h1 id={`${node._key}`}>{children}</h1>;
+    },
+    h2: (props: any) => {
+      const { node, children } = props;
+      return <h2 id={`${node._key}`}>{children}</h2>;
+    },
+    h3: (props: any) => {
+      const { node, children } = props;
+      return <h3 id={`${node._key}`}>{children}</h3>;
+    },
+  },
+};
+
+export default function PostBody({ headings, content }: Props) {
+  if (!content) return null;
+
   return (
     <div className={`mx-auto max-w-2xl ${markdownStyles.markdown ?? ""}`}>
-      {content && <PortableText value={content} />}
+      <Outline headings={headings} />
+      <PortableText value={content} components={components} />
     </div>
   );
 }
