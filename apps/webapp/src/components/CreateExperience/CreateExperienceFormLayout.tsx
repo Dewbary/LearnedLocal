3f5d4e95a1418d1f310cd8a3880f4dced4aee7d7
Brendan Dewberry <brendan.dewberry@domo.com>
2024-02-02 08:44:58 -0700

    fix navbar, and form submission

--- apps/webapp/src/components/CreateExperience/CreateExperienceFormLayout.tsx
+++ apps/webapp/src/components/CreateExperience/CreateExperienceFormLayout.tsx
@@ -7,7 +7,9 @@ const CreateExperienceFormLayout = () => {
   return (
     <>
       <div className="flex min-h-screen flex-col">
-        <NewNavBar />
+        <div className="bg-ll-grey">
+          <NewNavBar />
+        </div>
         <div className="flex flex-1 bg-ll-grey">
           <CreateExperienceFormBody />
         </div>
